b089a3f38015c50e442e60fd0ec5eba73eafbc7a
CodeQueen-AI <codeq209@gmail.com>
2026-02-12 15:53:19 +0500

    backend

---
+++ 01-Authentication/backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+// Connect MongoDB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .catch(err => console.log(err));

b38bc111b0f74d44b16918c409a1d43cedb553d6
CodeQueen-AI <codeq209@gmail.com>
2026-02-13 23:50:06 +0500

    update

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const fs = require('node:fs');
+// const fs = require('node:fs');
 
-// Write
-fs.writeFile("index.txt", 'Hello! How are you? ', function(err){
-    if(err) console.error(err);
-    else console.log("done")
+// // Write
+// fs.writeFile("index.txt", 'Hello! How are you? ', function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+// })
+
+// // Append
+// fs.appendFile("index.txt", 'I am fine!', function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+// })
+
+// // Rename
+// fs.rename("index.txt", 'hello.txt', function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+// })
+
+// // Copy File
+// fs.copyFile("hello.txt" , "./copy/copy.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log('Done')
+// })
+
+
+const http = require('http');
+const server = http.createServer(function(req, res){
+    res.end("Hello World");
 })
 
-// Append
-fs.appendFile("index.txt", 'I am fine!', function(err){
-    if(err) console.error(err);
-    else console.log("done")
-})
-
-// Rename
-fs.rename("index.txt", 'hello.txt', function(err){
-    if(err) console.error(err);
-    else console.log("done")
-})
-
-// Copy File
-fs.copyFile("hello.txt" , "./copy/copy.txt", function(err){
-    if(err) console.error(err);
-    else console.log('Done')
-})
+server.listen(3000)

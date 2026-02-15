20105c8ac0cfe420b48d6ebc2c5b120c9b121c4b
CodeQueen-AI <codeq209@gmail.com>
2026-02-15 22:35:39 +0500

    update

--- 03-Express.JS/scripts.js
+++ 03-Express.JS/scripts.js
@@ -1,7 +1,13 @@
 import express from 'express';
 const app = express()
 
+// Home Page
 app.get('/' , (req, res) => {
     res.send('Hello World')
 })
 app.listen(3000)
+
+// Routes - About Routes
+app.get('/about' , (req , res) => {
+    res.send('Hello World from About Page!!')
+})

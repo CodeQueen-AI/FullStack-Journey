084779ed7e564e935bf906efbd98900e7043f579
CodeQueen-AI <codeq209@gmail.com>
2026-02-15 22:27:24 +0500

    update

--- 03-Express.JS/scripts.js
+++ 03-Express.JS/scripts.js
@@ -1,7 +1,7 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 
-app.get('/' , function (req, res) {
+app.get('/' , (req, res) => {
     res.send('Hello World')
 })
 app.listen(3000)

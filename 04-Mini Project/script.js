f0c6a295985675d778aded90ea12324493529048
CodeQueen-AI <codeq209@gmail.com>
2026-02-16 16:20:32 +0500

    update

--- 04-Mini Project/script.js
+++ 04-Mini Project/script.js
@@ -2,11 +2,16 @@ import express from 'express';
 const app = express()
 
 // Parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send('Mini Project');
 });
 
+app.get("/profile/codequeen", (req, res) => {
+    res.send('CodeQueen Profile');
+});
+
 app.listen(3000)
+
